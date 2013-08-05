39fc524c5cf0f0e8aeb4253d406ac9d26a221ce3
VoR <vor@vor.com>
2013-08-05 09:59:13 -0400

    - Better control over focus. Keyboard will only focus when clicking on the title bar. - Cleaned up the code some.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,74 +28,71 @@ namespace OpenKeyboard{
 		private WindowInteropHelper wih = null;
 		private int focusStyle = 0;
 		private int unfocusStyle = 0;
-
-		private void Window_Loaded(object sender, RoutedEventArgs e){
-			this.wih = new WindowInteropHelper(this);
-
-			this.focusStyle = vWindow.GetWindowLong(this.wih.Handle,vWindow.GWL_EXSTYLE);
-			this.unfocusStyle = this.focusStyle | vWindow.WS_EX_NOACTIVATE;
-
-			string[] args = Environment.GetCommandLineArgs();
-			if(args.Length > 1) vLayout.Load(args[1],mainContainer,this);
-			else vLayout.Load("Default.xml",mainContainer,this);
-			
-			CreateJumpList();
-		}//func
 		
-		private void CreateJumpList(){
-			string[] ary = vLayout.GetLayoutList();
-			if(ary.Length == 0) return;
-			
-			JumpList jumpList = new JumpList();
-			JumpList.SetJumpList(Application.Current, jumpList);
-
-			for(int i=0; i < ary.Length; i++){
-				JumpTask jTask = new JumpTask();
-				jTask.Title = System.IO.Path.GetFileNameWithoutExtension(ary[i]);
-				jTask.Description = "Load this Keyboard Layout";
-				jTask.ApplicationPath = Assembly.GetEntryAssembly().Location;
-				jTask.Arguments = System.IO.Path.GetFileName(ary[i]);
-				
-				jumpList.JumpItems.Add(jTask);
-			}//for
-
-			jumpList.Apply();
-		}
-
 		public MainWindow(){ InitializeComponent(); }
 
-		private void Window_Activated_1(object sender, EventArgs e){
-			Console.WriteLine("active");
-			//SetWindowLong(this.wih.Handle, GWL_EXSTYLE, this.focusStyle);
-		}
+		#region Create JumpList
+			private void CreateJumpList(){
+				string[] ary = vLayout.GetLayoutList();
+				if(ary.Length == 0) return;
+			
+				JumpList jumpList = new JumpList();
+				JumpList.SetJumpList(Application.Current, jumpList);
 
-		private void Window_Deactivated_1(object sender, EventArgs e){
-			Console.WriteLine("lost focus");
-			vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.unfocusStyle);
-		}
+				for(int i=0; i < ary.Length; i++){
+					JumpTask jTask = new JumpTask();
+					jTask.Title = System.IO.Path.GetFileNameWithoutExtension(ary[i]);
+					jTask.Description = "Load this Keyboard Layout";
+					jTask.ApplicationPath = Assembly.GetEntryAssembly().Location;
+					jTask.Arguments = System.IO.Path.GetFileName(ary[i]);
+				
+					jumpList.JumpItems.Add(jTask);
+				}//for
+				jumpList.Apply();
+			}//func
+		#endregion
 
-		private void Window_MouseDown_1(object sender, MouseButtonEventArgs e){
-			Console.WriteLine("mdown");
-			vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.focusStyle);
-			this.Activate();
-		}
+		#region Window Events
+		private void Window_Activated(object sender, EventArgs e){}
+			private void Window_MouseDown(object sender, MouseButtonEventArgs e){}//func
 
-		/*GET Messages, On Left click on the title bar*/
-        protected override void OnSourceInitialized(EventArgs e){
-            base.OnSourceInitialized(e);
-            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
-            source.AddHook(WndProc);
-        }
+			private void Window_Loaded(object sender, RoutedEventArgs e){
+				this.wih = new WindowInteropHelper(this);
 
-        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled){
-			//WM_NCLBUTTONDOWN 
-			if(msg == 0x00A1){
-				Console.WriteLine(msg);
-				vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.focusStyle);
-				this.Activate();
-			}
-			return IntPtr.Zero;
-        }
-		 
+				this.focusStyle = vWindow.GetWindowLong(this.wih.Handle,vWindow.GWL_EXSTYLE);
+				this.unfocusStyle = this.focusStyle | vWindow.WS_EX_NOACTIVATE;
+
+				string[] args = Environment.GetCommandLineArgs();
+				if(args.Length > 1) vLayout.Load(args[1],mainContainer,this);
+				else vLayout.Load("Default.xml",mainContainer,this);
+			
+				CreateJumpList();
+			}//func
+
+			private void Window_Deactivated(object sender, EventArgs e){
+				//When window loses focus, prevent windows from getting focus back automaticly when clicking anything
+				vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.unfocusStyle);
+			}//func
+
+			/*GET Messages, Setup intercept from OS to control the focus of the window*/
+			protected override void OnSourceInitialized(EventArgs e){
+				base.OnSourceInitialized(e);
+				HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+				source.AddHook(WndProc);
+			}//func
+
+			private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled){
+				if(msg == 0x00A1){ //WM_NCLBUTTONDOWN 
+					Console.WriteLine(wParam);
+					if(wParam.ToInt64() == 0x0002){ //HT_CAPTION , only set focus if clicking on the title bar.
+						Console.WriteLine(wParam);
+						Console.WriteLine(msg);
+						vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.focusStyle);
+						this.Activate();
+					}//if
+				}//if
+				return IntPtr.Zero;
+			}//func
+		#endregion		 
 	}//cls
 }//ns
