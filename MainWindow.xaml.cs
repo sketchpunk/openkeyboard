463d05c2e2ed00e6d940b3c9400edb8de9bddc63
VoR <vor@vor.com>
2013-08-16 15:57:02 -0400

    - Appended to the context menu of the windows with items to switch keyboard profiles. - Some code clean up - Fade in when the app starts.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,52 +28,80 @@ namespace OpenKeyboard{
 		private WindowInteropHelper wih = null;
 		private int focusStyle = 0;
 		private int unfocusStyle = 0;
+		private Dictionary<int,string> kbLayouts = new Dictionary<int,string>();
 		
 		public MainWindow(){ InitializeComponent(); }
 
 		#region Create JumpList
-			private void CreateJumpList(){
+			private void CreateKBList(){
 				string[] ary = vLayout.GetLayoutList();
 				if(ary.Length == 0) return;
-			
+
+				//................................................
+				//Window Context Menu
+				IntPtr hMenu = vWindow.GetSystemMenu(this.wih.Handle, false);
+				
+				//Win8 Window Jumplist
 				JumpList jumpList = new JumpList();
 				JumpList.SetJumpList(Application.Current, jumpList);
 
-				for(int i=0; i < ary.Length; i++){
-					JumpTask jTask = new JumpTask();
-					jTask.Title = System.IO.Path.GetFileNameWithoutExtension(ary[i]);
-					jTask.Description = "Load this Keyboard Layout";
-					jTask.ApplicationPath = Assembly.GetEntryAssembly().Location;
-					jTask.Arguments = System.IO.Path.GetFileName(ary[i]);
+				//................................................
+				//Loop through file
+				int i;
+				string kbName = "",fileName = "", asmPath = Assembly.GetEntryAssembly().Location;
 				
+				for(i=0; i < ary.Length; i++){
+					//Save command handler id for menu and path for later system messaging listening
+					fileName = System.IO.Path.GetFileName(ary[i]);
+					kbName = System.IO.Path.GetFileNameWithoutExtension(ary[i]);
+					this.kbLayouts.Add(1000+i+1,fileName);
+
+					//Create items in context menu
+					vWindow.InsertMenu(hMenu, i, vWindow.MF_BYPOSITION, 1000+i+1, kbName);
+
+					//Create jumplist item
+					JumpTask jTask = new JumpTask();
+					jTask.Title = kbName;
+					jTask.Description = "Load this Keyboard Layout";
+					jTask.ApplicationPath = asmPath;
+					jTask.Arguments = fileName;
 					jumpList.JumpItems.Add(jTask);
 				}//for
+
+				//................................................
+				//Create Seperator
+				vWindow.InsertMenu(hMenu,i,vWindow.MF_SEPARATOR | vWindow.MF_BYPOSITION,1000+i+1,null);
+
+				//Apply Jump Items
 				jumpList.Apply();
-			}//func
+			}//for
 		#endregion
 
 		#region Window Events
-		private void Window_Activated(object sender, EventArgs e){}
+			private void Window_Activated(object sender, EventArgs e){}
 			private void Window_MouseDown(object sender, MouseButtonEventArgs e){}//func
 
 			private void Window_Loaded(object sender, RoutedEventArgs e){
+				//Setup focus styles
 				this.wih = new WindowInteropHelper(this);
-
 				this.focusStyle = vWindow.GetWindowLong(this.wih.Handle,vWindow.GWL_EXSTYLE);
 				this.unfocusStyle = this.focusStyle | vWindow.WS_EX_NOACTIVATE;
 
+				//Check which keyboard profile to load in.
 				string[] args = Environment.GetCommandLineArgs();
 				if(args.Length > 1) vLayout.Load(args[1],mainContainer,this);
 				else vLayout.Load("Default.xml",mainContainer,this);
 			
-				CreateJumpList();
+				CreateKBList();
 			}//func
 
 			private void Window_Deactivated(object sender, EventArgs e){
 				//When window loses focus, prevent windows from getting focus back automaticly when clicking anything
 				vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.unfocusStyle);
 			}//func
-
+		#endregion
+	 
+		#region Window Messaging
 			/*GET Messages, Setup intercept from OS to control the focus of the window*/
 			protected override void OnSourceInitialized(EventArgs e){
 				base.OnSourceInitialized(e);
@@ -82,17 +110,23 @@ namespace OpenKeyboard{
 			}//func
 
 			private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled){
-				if(msg == 0x00A1){ //WM_NCLBUTTONDOWN 
-					Console.WriteLine(wParam);
-					if(wParam.ToInt64() == 0x0002){ //HT_CAPTION , only set focus if clicking on the title bar.
-						Console.WriteLine(wParam);
-						Console.WriteLine(msg);
-						vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.focusStyle);
-						this.Activate();
-					}//if
-				}//if
+				switch(msg){
+					//............................................................
+					case vWindow.WM_NCLBUTTONDOWN:
+						if(wParam.ToInt64() == vWindow.HT_CAPTION){ //only set focus if clicking on the title bar.
+							vWindow.SetWindowLong(this.wih.Handle, vWindow.GWL_EXSTYLE, this.focusStyle);
+							this.Activate();
+						}//if
+					break;
+					
+					//............................................................
+					case vWindow.WM_SYSCOMMAND:
+						int wparam = wParam.ToInt32();
+						if(this.kbLayouts.ContainsKey(wparam)) vLayout.Load(this.kbLayouts[wparam],mainContainer,this);
+					break;
+				}//switch
 				return IntPtr.Zero;
 			}//func
-		#endregion		 
+		#endregion
 	}//cls
 }//ns
--- vWindow.cs
+++ vWindow.cs
@@ -7,6 +7,12 @@ namespace OpenKeyboard{
 		#region Constants
 			public const int WS_EX_NOACTIVATE = 0x08000000;
 			public const int GWL_EXSTYLE = -20;
+			public const int WM_NCLBUTTONDOWN = 0x00A1;
+			public const int HT_CAPTION = 0x0002;
+
+			public const int WM_SYSCOMMAND = 0x112;
+			public const int MF_BYPOSITION = 0x400;
+			public const int MF_SEPARATOR = 0x800;
 		#endregion
 
 		#region DLL Imports
@@ -15,6 +21,12 @@ namespace OpenKeyboard{
 
 			[DllImport("user32", SetLastError = true)]
 			public extern static int SetWindowLong(IntPtr hwnd, int nIndex, int dwNewValue);
+
+			[DllImport("user32", SetLastError = true)]
+			public extern static IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+			
+			[DllImport("user32", SetLastError = true)]
+			public extern static bool InsertMenu(IntPtr hMenu,Int32 wPosition, Int32 wFlags, Int32 wIDNewItem,string lpNewItem);    
 		#endregion
 	}//cls
 }//ns
